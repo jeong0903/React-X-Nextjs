da54e4324cb71c72438595c37469982a3d6964c3
JeongHyeon Park <9193jane@gmail.com>
2024-01-18 00:20:11 +0900

    Next.js 13-16 삭제

--- src/app/Control.js
+++ src/app/Control.js
@@ -1,10 +1,11 @@
 "use client";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import "./globals.css";
 
 export function Control() {
   const params = useParams();
+  const router = useRouter();
   const id = params.id;
   return (
     <ul>
@@ -13,10 +14,23 @@ export function Control() {
       </li>
       {id ? (
         <>
-          <li><Link href={"/update/"+id}>Update</Link></li>
-          <li><input type="button" value="delete" /></li>
-        </>
-      ) : null}
+          <li>
+            <Link href={"/update/" + id}>Update</Link>
+          </li>
+          <li>
+            <input
+              type="button"
+              value="delete"
+              onClick={() => {
+                const options = { method: "DELETE" };
+                fetch(`http://localhost:9999/topics/${id}`, options)
+                  .then((resp) => resp.json())
+                  .then((result) => {
+                    router.push("/");
+                    router.refresh();
+                  });
+              }}/></li>
+        </>) : null}
     </ul>
   );
 }
