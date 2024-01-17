261acdeee3009a919d0eb0af10c1e59d057cefd9
JeongHyeon Park <9193jane@gmail.com>
2024-01-18 00:04:45 +0900

    Next.js 13-15 글수정(update)

---
+++ src/app/update/[id]/page.js
@@ -0,0 +1,62 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Update(props) {
+  const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const id = props.params.id;
+  async function refresh() {
+    const resp = await fetch(`http://localhost:9999/topics/${id}`);
+    const topic = await resp.json();
+    setTitle(topic.title);
+    setBody(topic.body);
+  }
+  useEffect(() => {
+    refresh();
+  }, []);
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const body = e.target.body.value;
+        const resp = await fetch(`http://localhost:9999/topics/${id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title, body }),
+        });
+        const topic = await resp.json();
+        router.push(`/read/${topic.id}`);
+        router.refresh();
+      }}
+    >
+      <h2>Update</h2>
+      <p>
+        <input
+          type="text"
+          name="title"
+          placeholder="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </p>
+      <p>
+        <textarea
+          name="body"
+          placeholder="body"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+      </p>
+      <p>
+        <input type="submit" value="update" />
+      </p>
+    </form>
+  );
+}
